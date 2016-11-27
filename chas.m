0cafc7b5b408be31b1bb598eb9965287d09a5ebc
LvRandom <845386473@qq.com>
2016-11-27 18:31:51 +0800

    Revert "数值实验代码"
    This reverts commit 87dd5cf37bbfd90f094ef7a915ac744f6f4426a0.

--- chas.m
+++
@@ -1,8 +0,0 @@
-function y =chas(xi)
-%xi为插值数据点的向量形式
-t=0; 
-k=length(xi);
-for i=1:k
-    t = t + f(xi(i))/w(k,xi,i);
-end
-y = t;
--- w.m
+++
@@ -1,8 +0,0 @@
-function y = w(k,xi,i)
-t=1;
-for j=1:k
-    if(j~=i)
-        t=t*(xi(i)-xi(j));
-    end
-end
-y=t;
